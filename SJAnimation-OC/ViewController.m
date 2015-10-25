148aeb4b3ad2816561ada2c5b3358c05efbdb9ed
shejunzhou <965678322@qq.com>
2015-10-25 19:19:39 +0800

    CALayer的position属性变化时的动画效果。 CABasicAnimation 的transform.scale的动画效果。

--- SJAnimation-OC/ViewController.m
+++ SJAnimation-OC/ViewController.m
@@ -10,6 +10,7 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIView *basicView;
 @end
 
 @implementation ViewController
@@ -17,11 +18,52 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self drawCustomLayer];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event{
+    UITouch *touch=[touches anyObject];
+    CALayer *layer=[self.view.layer.sublayers lastObject];
+    CGFloat width=layer.bounds.size.width;
+    if (width==40) {
+        width=40*4;
+    }else{
+        width=40;
+    }
+    layer.bounds=CGRectMake(0, 0, width, width);
+    layer.position=[touch locationInView:self.view];
+    layer.cornerRadius=width/2;
+    layer.hidden = NO;
+}
+
+- (IBAction)tappedButtonAction:(id)sender {
+    CABasicAnimation *basicAnimation = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+    basicAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut];
+    basicAnimation.duration = 0.5;
+    basicAnimation.repeatCount = 1;
+    basicAnimation.autoreverses = YES;
+    basicAnimation.fromValue = [NSNumber numberWithFloat:1.0];
+    basicAnimation.toValue = [NSNumber numberWithFloat:1.2];
+    [self.basicView.layer addAnimation:basicAnimation forKey:nil];
+}
+
+- (void)drawCustomLayer {
+    CGFloat width = 40.0f;
+    CGSize size = [UIScreen mainScreen].bounds.size;
+    CALayer *subLayer = [[CALayer alloc] init];//创建子layer
+    subLayer.backgroundColor = [UIColor orangeColor].CGColor;//设置背景颜色,由于QuartzCore是跨平台框架，无法直接使用UIColor
+    subLayer.position = CGPointMake(size.width/2.0, size.height/2.0);//设置中心点
+    subLayer.bounds = CGRectMake(0, 0, width, width);//设置大小
+    subLayer.cornerRadius = width/2.0;//设置圆角,当圆角半径等于矩形的一半时看起来就是一个圆形
+    subLayer.shadowColor = [UIColor grayColor].CGColor;//设置阴影
+    subLayer.shadowOffset = CGSizeMake(2, 2);//设置阴影偏移量
+    subLayer.shadowOpacity = 0.8f;//设置阴影透明度
+    [self.view.layer addSublayer:subLayer];
 }
 
 @end
